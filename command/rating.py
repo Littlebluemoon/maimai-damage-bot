0ca7a2c9285b2f1a627a6ee0e3cc1976110565e7
Littlebluemoon <vietthanh16122012@gmail.com>
2026-01-22 11:58:16 +0700

    Rating rework for AP

--- command/rating.py
+++ command/rating.py
@@ -23,15 +23,16 @@ async def rating(ctx, cc, rate):
 				break
 			elif achv_rate == ACHV[i]:
 				break
-		await ctx.send(f'''
-			A chart with a difficulty of {cc} and a score of {rate}% would award you **{math.floor(min(1005000, achv_rate) * RANK[i] * float(cc) / 10000000)}** rating.''')
+		msg = f'A chart with a difficulty of {cc} and a score of {rate}% would award you **{math.floor(min(1005000, achv_rate) * RANK[i] * float(cc) / 10000000)}** rating.'''
+		if int(rate.replace('.', '')) >= 1005000:
+			msg += f'\n- If you achieved an All Perfect, you will get **{math.floor(min(1005000, achv_rate) * RANK[i] * float(cc) / 10000000) + 1}** rating instead.'
 
 
 @commands.command()
 async def target(ctx, tgt_rating):
 	if int(tgt_rating) <= 0:
 		await ctx.send(f"Rating to target should be higher than 0")
-	elif int(tgt_rating) > 337:
+	elif int(tgt_rating) > 338:
 		await ctx.send(f"We will need a 15.1 for that")
 	else:
 		data = generate_rating_table(tgt_rating)
